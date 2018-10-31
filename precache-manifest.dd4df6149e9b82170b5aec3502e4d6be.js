b66859409a976bee0cf1d6c95bd867c58a809373
Oleg Buyalski <oleg.buyalski@gmail.com>
2018-10-31 20:20:05 +0300

    Updates

---
+++ precache-manifest.dd4df6149e9b82170b5aec3502e4d6be.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "b1f4b28c5da521625942",
+    "url": "/weather-app/static/css/main.84572bbf.chunk.css"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  },
+  {
+    "revision": "891ec9405c450b734c0d",
+    "url": "/weather-app/static/js/1.891ec940.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "b1f4b28c5da521625942",
+    "url": "/weather-app/static/js/main.b1f4b28c.chunk.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "891ec9405c450b734c0d",
+    "url": "/weather-app/static/css/1.f562991f.chunk.css"
+  },
+  {
+    "revision": "4a49099dffca3c25115b3c698960cb15",
+    "url": "/weather-app/index.html"
+  }
+];
