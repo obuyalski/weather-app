e37b3896569ba0cd39b06e34941e3f56c0dc8e9a
Oleg Buyalski <oleg.buyalski@gmail.com>
2018-10-31 19:45:34 +0300

    Setup map with search by cities and countries

--- src/App.js
+++ src/App.js
@@ -1,28 +1,131 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import MapGL, { Marker } from 'react-map-gl';
+import Geosuggest from 'react-geosuggest';
+import CityPin from './components/CityPin';
+import {defaultMapStyle} from './styles/map-style.js';
+import WeatherDisplay from './components/WeatherDisplay';
+import { fetchWeather } from './actions/weatherAction';
+import './styles/App.css';
+import './styles/geosuggest.css';
+
+const TOKEN = "pk.eyJ1IjoiemlkYWJhaCIsImEiOiJjam51ZjE3N2QwM3VqM3dudnl5dXoyb213In0.wPZNMnjjPWu6zN4aHPjg2w";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mapStyle: defaultMapStyle,
+      viewport: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8
+      }
+    };
+  }
+
   render() {
+    const { viewport, latCity, longCity, mapStyle } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="fixed-search">
+          <Geosuggest
+            placeholder="Поиск на карте"
+            onViewportChange={this.updateViewport}
+            onSuggestSelect={this.onSuggestSelect}
+            location={new window.google.maps.LatLng(53.558572, 9.9278215)}
+            radius={20} />
+          <WeatherDisplay />
+        </div>
+        <MapGL
+          {...viewport}
+          mapStyle={mapStyle}
+          mapboxApiAccessToken={TOKEN}
+          onViewportChange={(viewport) => this.onViewportChange(viewport)}
+          style={{ textAlign: 'left' }}
+        >
+        {latCity && longCity &&
+          <Marker
+            latitude={latCity}
+            longitude={longCity}
+            key="marker"
           >
-            Learn React
-          </a>
-        </header>
+            <CityPin size={20} />
+          </Marker>
+        }
+        </MapGL>
       </div>
     );
   }
+
+  updateViewport = (viewport) => {
+    this.setState({ viewport });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize);
+    this.resize();
+  }
+
+  resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: this.props.width || window.innerWidth,
+        height: this.props.height || window.innerHeight
+      }
+    });
+  };
+
+  onViewportChange = (viewport) => {
+    this.setState({
+      viewport: {
+        ...viewport
+      }
+    })
+  };
+
+  onSuggestSelect = (suggest) => {
+    if (suggest) {
+      const { description, location } = suggest;
+
+      this.getWeather(description);
+      this.setState({
+          viewport: {
+            ...this.state.viewport,
+            zoom: 12,
+            latitude: location.lat,
+            longitude: location.lng,
+          },
+          latCity: location.lat,
+          longCity: location.lng
+      });
+    }
+  }
+  
+  getWeather = (description) => {
+    this.props.fetchWeather(description);
+  }
+
 }
 
-export default App;
+const mapStateToProps = store => {
+  return {
+    store: store
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchWeather: description => dispatch(fetchWeather(description))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
